3f5f68396c95b15ce46a504fea9e78ef40b202d7
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-03-13 17:00:24 -0300

    Fazendo um Controller com VRaptor

---
+++ src/main/java/br/com/caelum/vraptor/controller/ProdutoController.java
@@ -0,0 +1,13 @@
+package br.com.caelum.vraptor.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Path;
+
+@Controller
+public class ProdutoController {
+
+	@Path("/")
+	public void inicio() {
+		
+	}
+}
